5f69c4b0cf47f66c6aa668601c0057d3dc8d4da2
baderda <baderda@in.tum.de>
2017-03-08 16:25:14 +0100

    added example fct with docu comment tags

--- {{cookiecutter.repo_name}}/R/{{ cookiecutter.repo_name }}.R
+++ {{cookiecutter.repo_name}}/R/{{ cookiecutter.repo_name }}.R
@@ -3,3 +3,37 @@
 #' @docType package
 #' @name {{ cookiecutter.repo_name }}
 NULL
+
+#
+# Write functions only and document them with roxygen-styled comments.
+# Example from FraseR package below.
+#
+
+#' 
+#' @title Find rare splicing events within RNA-seq data
+#' @description This function performs a default analysis of RNA-seq data
+#'             
+#' @param settings A FraseRSetting object with all the information 
+#'             how and what to count
+#' @param internBPPARAM A BiocParallel param object to configure the 
+#'             parallel backend of the internal loop for counting
+#'             
+#' @return FraseRDataSet 
+#' @export
+#' @examples
+#'   fds <- FraseR(createTestFraseRSettings())
+#'   plotSampleResults(fds)
+FraseR <- function(settings, internBPPARAM=SerialParam()){
+  
+  # Check input
+  stopifnot(class(settings) == "FraseRSettings")
+  stopifnot(is(internBPPARAM, "BiocParallelParam"))
+  
+  # count data
+  fds <- countRNAData(settings, internBPPARAM=internBPPARAM)
+  #...
+  # return final analysis
+  return(fds)
+}
+
+
