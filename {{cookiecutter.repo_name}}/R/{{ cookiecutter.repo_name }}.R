ab702d46552aba7bfe1dcb91daee1b8129a3e42b
Jacob Carey <JCarey@telligen.com>
2017-03-09 10:33:46 -0500

    Change example function simple add function

--- {{cookiecutter.repo_name}}/R/{{ cookiecutter.repo_name }}.R
+++ {{cookiecutter.repo_name}}/R/{{ cookiecutter.repo_name }}.R
@@ -6,34 +6,18 @@ NULL
 
 #
 # Write functions only and document them with roxygen-styled comments.
-# Example from FraseR package below.
+# Example below taken from http://r-pkgs.had.co.nz/man.html
 #
 
+#' Add together two numbers.
 #' 
-#' @title Find rare splicing events within RNA-seq data
-#' @description This function performs a default analysis of RNA-seq data
-#'             
-#' @param settings A FraseRSetting object with all the information 
-#'             how and what to count
-#' @param internBPPARAM A BiocParallel param object to configure the 
-#'             parallel backend of the internal loop for counting
-#'             
-#' @return FraseRDataSet 
-#' @export
+#' @param x A number.
+#' @param y A number.
+#' @return The sum of \code{x} and \code{y}.
 #' @examples
-#'   fds <- FraseR(createTestFraseRSettings())
-#'   plotSampleResults(fds)
-FraseR <- function(settings, internBPPARAM=SerialParam()){
-  
-  # Check input
-  stopifnot(class(settings) == "FraseRSettings")
-  stopifnot(is(internBPPARAM, "BiocParallelParam"))
-  
-  # count data
-  fds <- countRNAData(settings, internBPPARAM=internBPPARAM)
-  #...
-  # return final analysis
-  return(fds)
+#' add(1, 1)
+#' add(10, 1)
+#' @export
+add <- function(x, y) {
+      x + y
 }
-
-
